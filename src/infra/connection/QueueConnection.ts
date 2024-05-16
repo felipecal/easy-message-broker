bc0856d97bd6c3f2bdac3b57d6f4bd4d291a30b5
Felipe <fcalldas@gmail.com>
2024-05-16 17:16:04 -0300

    ♻️ refactor: remove dead repositorys

--- src/infra/connection/QueueConnection.ts
+++
@@ -1,9 +0,0 @@
-import amqp from 'amqplib';
-
-export default class QueueConnection {
-  constructor(private url: string){}
-
-  async connect(): Promise<void> {
-    await amqp.connect(`amqp://${this.url}`);
-  }
-}
